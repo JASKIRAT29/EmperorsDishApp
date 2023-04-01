be915c485749bd5c86327263c7d5039ba261c26c
JASKIRAT29 <jaskiratkr29@gmail.com>
2023-04-01 13:40:43 -0400

    changes menu11

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
+        <div id="root" className="App">
             <Router>
                 <Nav />
                 <Routes>
