7af0d93ac2a2a09195247e2cc9506d3abaa9c3c2
Dexter Dick <@dexterdick@hotmail.com>
2023-04-02 07:17:55 -0400

    started coding shopping cart

--- client/src/pages/Order.js
+++ client/src/pages/Order.js
@@ -1,7 +1,47 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
-const Order = () => {
-    return <div> Cart</div>;
+const Order = ({ cart }) => {
+    return (
+        <div>
+            <h3 className="cart-title">Shopping Cart {console.log(cart)}</h3>
+            <article className="cart">
+                <div className="row">
+                    <div class="cart_img">
+                        <img src="images/1.png" alt=""></img>
+                        <p>Fish and rice </p>
+                    </div>
+                    <div>
+                        <Button variant="outlined" size="small">
+                            +
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            sx={{ margin: 1 }}
+                            size="small"
+                        >
+                            1
+                        </Button>
+                        <Button variant="outlined" size="small">
+                            -
+                        </Button>
+                    </div>
+                    <div>
+                        <span className="price-span">$89</span>
+                        <Button className="remove" variant="contained">
+                            Remove
+                        </Button>
+                    </div>
+                </div>
+
+                <hr></hr>
+                <div class="total">
+                    <span>Total Price of your Cart: </span>
+                    <span>$89</span>
+                </div>
+            </article>
+        </div>
+    );
 };
 
 export default Order;
